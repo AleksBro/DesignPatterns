40ad9717eadfd31783e82f25d7926c575963b782
AlekBro <brova.aleksandr@apriorit.com>
2016-11-24 19:06:38 +0200

    Starting ChainOfResponsibility.

--- SoftwareDesignPatterns/SoftwareDesignPatterns.WinFormsApp/Form1.Designer.cs
+++ SoftwareDesignPatterns/SoftwareDesignPatterns.WinFormsApp/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.TestCreationalPatternsFactoryMethod_button = new System.Windows.Forms.Button();
+            this.TestBehavioralPatternsChainOfResponsibility_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TestCreationalPatternsFactoryMethod_button
             // 
-            this.TestCreationalPatternsFactoryMethod_button.Location = new System.Drawing.Point(58, 65);
+            this.TestCreationalPatternsFactoryMethod_button.Location = new System.Drawing.Point(41, 51);
             this.TestCreationalPatternsFactoryMethod_button.Name = "TestCreationalPatternsFactoryMethod_button";
             this.TestCreationalPatternsFactoryMethod_button.Size = new System.Drawing.Size(191, 24);
             this.TestCreationalPatternsFactoryMethod_button.TabIndex = 0;
@@ -41,11 +42,21 @@
             this.TestCreationalPatternsFactoryMethod_button.UseVisualStyleBackColor = true;
             this.TestCreationalPatternsFactoryMethod_button.Click += new System.EventHandler(this.TestCreationalPatternsFactoryMethod_button_Click);
             // 
+            // TestBehavioralPatternsChainOfResponsibility_button
+            // 
+            this.TestBehavioralPatternsChainOfResponsibility_button.Location = new System.Drawing.Point(41, 97);
+            this.TestBehavioralPatternsChainOfResponsibility_button.Name = "TestBehavioralPatternsChainOfResponsibility_button";
+            this.TestBehavioralPatternsChainOfResponsibility_button.Size = new System.Drawing.Size(217, 23);
+            this.TestBehavioralPatternsChainOfResponsibility_button.TabIndex = 1;
+            this.TestBehavioralPatternsChainOfResponsibility_button.Text = "BehavioralPatterns.ChainOfResponsibility";
+            this.TestBehavioralPatternsChainOfResponsibility_button.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(786, 492);
+            this.Controls.Add(this.TestBehavioralPatternsChainOfResponsibility_button);
             this.Controls.Add(this.TestCreationalPatternsFactoryMethod_button);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button TestCreationalPatternsFactoryMethod_button;
+        private System.Windows.Forms.Button TestBehavioralPatternsChainOfResponsibility_button;
     }
 }
 
---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/BehavioralPatterns/ChainOfResponsibility/wikipedia.org/ConcreteHandler3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns.BehavioralPatterns.ChainOfResponsibility.wikipedia.org
+{
+    /// <summary>
+    /// The 'ConcreteHandler3' class
+    /// </summary>
+    class ConcreteHandler3 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 20 && request < 30)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                  this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}
